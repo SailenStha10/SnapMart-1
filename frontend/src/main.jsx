3c5f17e960949868832da06a86c28aefeeb6182c
SailenStha <saienstha@gmail.com>
2026-05-18 21:33:29 +0545

    frontend setup and error solving in app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles/index.css'
+import App from './App.jsx'
+import './index.css'
+import { AuthProvider } from './context/AuthContext'
 
-const root = createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
